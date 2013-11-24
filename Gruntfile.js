9c8ab29f08f967ae7507fd18791e44704e3493ed
Yoryos Valotasios <valotas@gmail.com>
2013-11-24 17:36:19 +0100

    Added grunt-ssh and make use of it for deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,8 @@
 module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-wintersmith');
+  //grunt.loadNpmTasks('grunt-scp');
+  grunt.loadNpmTasks('grunt-ssh');
+  
 
   grunt.initConfig({
     wintersmith: {
@@ -9,9 +12,26 @@ module.exports = function (grunt) {
           action: 'preview'
         }
       }
+    },
+    sftp: {
+      build: {
+        files: {
+          './': 'build/**'
+        },
+        options: {
+          path: '/var/www/valotas.com/',
+          srcBasePath: 'build/',
+          host: 'pi',
+          createDirectories: true,
+          username: 'valotas',
+          privateKey: grunt.file.read("/home/valotas/.ssh/id_rsa"),
+          //privateKey: require('fs').readFileSync('/home/valotas/.ssh/id_rsa'),
+          passphrase: grunt.file.read("/home/valotas/.ssh/id_rsa.pass")
+        }
+      }
     }
   });
 
   grunt.registerTask('preview', ['wintersmith:preview']);
-  grunt.registerTask('deploy', ['wintersmith:build']);
+  grunt.registerTask('deploy', ['wintersmith:build', 'sftp:build']);
 };
