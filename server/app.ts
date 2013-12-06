75940a87de98855e084de778b213a26635cfafc8
George Valotasios <george@wuala.com>
2013-12-06 15:02:02 +0100

    A simple modular app running

--- server/app.ts
+++ server/app.ts
@@ -5,25 +5,38 @@
 import express = require('express')
 import less = require('less-middleware')
 import path = require('path')
+import articles = require('articles')
 
 var contents = path.normalize(__dirname + '/../contents'),
   app = express();
 
 app
-  .use(function (req: express.Request, resp: express.Response, next?: Function) {
-    console.log('%s %s %s', contents, req.method, req.originalUrl);
-    next();
-  })
+//  .use(function (req: express.Request, resp: express.Response, next?: Function) {
+//    console.log('%s %s %s', contents, req.method, req.originalUrl);
+//    next();
+//  })
+
+  .use(express.logger())
+
+  //Add less support to our server
   .use(less({ src: contents }))
+
+  // Server static content
   .use(express.static(contents))
+
+  // Add an article handler:
+  .use(articles)
+
+  //Add a simple handler for /
   .get('/', function (req: express.Request, resp: express.Response) {
     var body = 'Hello man!';
     resp.contentType('text/plain')
       .header('Content-Length', '' + body.length)
       .end(body);
   })
+
+  //Run the server on port 8080
   .listen(8080, function () {
     console.log('Listening on 8080');
   });
 
-export var App = app;
