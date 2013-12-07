9fce77959053f57e0e7a03ec0fff61e0d1eaa23b
Yoryos Valotasios <valotas@gmail.com>
2013-12-07 16:36:52 +0100

    Experimenting with real tests

--- server/articles.spec.ts
+++ server/articles.spec.ts
@@ -1,10 +1,15 @@
 ///<reference path='../d.ts/DefinitelyTyped/mocha/mocha.d.ts' />
 ///<reference path='../d.ts/DefinitelyTyped/chai/chai.d.ts' />
 
+import articles = require('articles')
 var expect = require('chai').expect;
 
-describe('articles specs', () => {
-  it('should not fail!', () => {
-    expect(true).to.equal(true);
-  });
+describe('articles', () => {
+  describe('ArticleUrlParam', () => {
+    it('should throw an error when trying to construct it without paramters', () => {
+      expect(() => {
+        new articles.ArticleUrlParams(null);
+      }).to.throw(/Can not work with a null paramter object/)
+    });
+  })
 });
--- server/articles.ts
+++ server/articles.ts
@@ -1,6 +1,29 @@
 ///<reference path='../d.ts/DefinitelyTyped/express/express.d.ts' />
 ///<reference path='../d.ts/DefinitelyTyped/node/node.d.ts' />
 
+class ArticleUrlParam {
+  private _title: string;
+  private _year: number;
+  private _month: number;
+
+  constructor(param: any) {
+    if (!(param)) {
+      throw 'Can not work with a null parameter object'
+    }
+    this._month = param.month || null;
+    this._year = param.year || null;
+    this._title = param.title || null;
+
+    if (this._title == null) {
+      throw 'Title can not be null';
+    }
+  }
+
+  path():string {
+    return 'asd';
+  }
+}
+
 import express = require('express')
 
 var app = express();
