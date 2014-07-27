2d9a65ac2b00b0a08aafc272d996b64b753c7db0
Yoryos Valotasios <valotas@gmail.com>
2014-07-27 13:45:25 +0200

    Add an article object to given pages

--- assemble/article-middleware.js
+++ assemble/article-middleware.js
@@ -2,8 +2,21 @@
 
 'use strict';
 
+var moment = require('moment');
+
+function Article(page) {
+
+  this.moment = function () {
+    return moment(page.date);
+  };
+
+  this.date = function (format) {
+    return this.moment().format(format || 'DD/MM/YYYY');
+  };
+}
+
 function addArticleInfo (page) {
-  page.article = 'xxx';
+  page.article = new Article(page);
 }
 
 var middleware = function (param, next) {
@@ -15,4 +28,5 @@ var middleware = function (param, next) {
 middleware.options = {
   stage: 'render:pre:page'
 };
+
 module.exports = middleware;
--- assemble/article-middleware.spec.js
+++ assemble/article-middleware.spec.js
@@ -1,10 +1,45 @@
 /*jshint node:true*/
-/*globals describe, it, expect*/
+/*globals describe, it, expect, beforeEach*/
 
 'use strict';
 
+var middleware = require('./article-middleware');
+
+function noop() {}
+
+function runMiddleware(page) {
+  var params = {
+    context: {
+      page: page
+    }
+  };
+  middleware(params, noop);
+}
+
 describe('article-middleware', function () {
-  it('should fail', function () {
-    expect(1).toEqual(2);
+
+  it('should add an article property to the given page', function () {
+    var page = {};
+    runMiddleware(page);
+    expect(page.article).not.toBeUndefined();
+  });
+
+  describe('Article', function () {
+    var page = {};
+
+    beforeEach(function () {
+      runMiddleware(page);
+    });
+
+    it('should provide a function to get the published date', function (){
+      expect(page.article.date).not.toBeUndefined();
+      page.date = '22 Aug 2013';
+      expect(page.article.date()).toEqual('22/08/2013');
+    });
+
+    it('should allow to get the published date formated', function (){
+      page.date = '22 Aug 2013';
+      expect(page.article.date('YY-MM-DD')).toEqual('13-08-22');
+    });
   });
 });
