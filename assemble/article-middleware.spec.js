955a1ad4f319a4d238aab4a6fe658d30aed910be
Yoryos Valotasios <valotas@gmail.com>
2014-07-27 13:24:59 +0200

    Added failing test for article-middleware

---
+++ assemble/article-middleware.spec.js
@@ -0,0 +1,10 @@
+/*jshint node:true*/
+/*globals describe, it, expect*/
+
+'use strict';
+
+describe('article-middleware', function () {
+  it('should fail', function () {
+    expect(1).toEqual(2);
+  });
+});
