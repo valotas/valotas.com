8c9ecf33d8eab428e9a6b45438e95a7ecf8f014f
Yoryos Valotasios <valotas@gmail.com>
2014-07-27 17:38:44 +0200

    Added tests for our engine and modified it

--- assemble/engine.js
+++ assemble/engine.js
@@ -1,15 +1,25 @@
 /*jshint node:true*/
+
 "use strict";
 
 var jade = require('jade');
 
-var render = function (tmpl, opt, cb) {
-  var template = jade.compileFile(opt.jade.layout, {
+function render (tmpl, opt, cb) {
+  var options = {
     filename: opt.page.src,
     pretty: true
-  });
+  };
+  var page = opt.page;
+  var template = null;
+  if (page.md) {
+    var templateFile = page.template || opt.jade.defaultTemplate;
+    options.filename = opt.jade.templates + templateFile;
+    template = jade.compileFile(options.filename, options);
+  } else {
+    template = jade.compile(tmpl, options);
+  }
   cb(null, template(opt.page));
-};
+}
 
 module.exports = {
   render: render
---
+++ assemble/engine.spec.js
@@ -0,0 +1,44 @@
+/*jshint node:true*/
+/*globals describe, it, expect, beforeEach, jasmine*/
+
+'use strict';
+
+var engine = require('./engine');
+
+describe('engine', function () {
+  var cb;
+
+  beforeEach(function () {
+    cb = jasmine.createSpy("cb");
+  });
+
+  it('should try to render the given template as jade', function () {
+    var options = {
+      page: {
+        layout: 'layout.jade',
+        src: 'path/to/the/file'
+      }
+    };
+
+    engine.render('h1 jade #{src}', options, cb);
+    expect(cb).toHaveBeenCalledWith(null, '\n<h1>jade path/to/the/file</h1>');
+  });
+
+  describe('markdown', function () {
+    it('should try to render giben page.template when page is markdown', function () {
+      var options = {
+        jade: {
+          templates: 'src/templates/'
+        },
+        page: {
+          md: true,
+          template: 'layout.jade',
+          src: 'path/to/file'
+        }
+      };
+
+      engine.render('markdown text', options, cb);
+      expect(cb).toHaveBeenCalled();
+    });
+  });
+});
