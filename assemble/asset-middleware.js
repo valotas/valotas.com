f625028e1da61f4814429e3fd52119bc078496ce
Yoryos Valotasios <valotas@gmail.com>
2014-08-26 23:25:33 +0200

    Added a dummy implementation and some failing tests

--- assemble/asset-middleware.js
+++ assemble/asset-middleware.js
@@ -1,3 +1,21 @@
 /*jshint node:true*/
 
 'use strict';
+
+//var md5 = require('MD5');
+
+var asset = function (filename) {
+  return 'md5-hash-of-' + filename;
+};
+
+var middleware = function (param, next) {
+  param.context.asset = asset;
+  next();
+};
+
+middleware.options = {
+  stage: 'render:pre:page'
+};
+
+
+module.exports = middleware;
--- assemble/asset-middleware.spec.js
+++ assemble/asset-middleware.spec.js
@@ -3,10 +3,30 @@
 
 'use strict';
 
+var path = require('path');
 var middleware = require('./asset-middleware');
 
+function noop() {}
+
+function runMiddlewareAndGetAsset() {
+  var params = {
+    context: {}
+  };
+  middleware(params, noop);
+  return params.context.asset;
+}
+
 describe('asset-middleware', function () {
-  it('should fail for the moment', function () {
-    expect(middleware).toEqual(null);
+  it('should add an apply function to the given context', function () {
+    var asset = runMiddlewareAndGetAsset();
+    expect(asset).not.toBeUndefined();
   });
+  
+  describe('asset function', function () {
+    it('should return the md5 of the given file', function () {
+      var asset = runMiddlewareAndGetAsset();
+      expect(asset(path.resolve(__filename))).toEqual('asdkldjf');
+    });
+  });
+  
 });
