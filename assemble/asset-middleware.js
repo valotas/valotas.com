195f2a56d34260f0274972a24ea526c0436ab133
Yoryos Valotasios <valotas@gmail.com>
2014-08-26 23:48:30 +0200

    Fixed failing test

--- assemble/asset-middleware.js
+++ assemble/asset-middleware.js
@@ -6,14 +6,16 @@ var fs = require('fs');
 var path = require('path');
 var md5 = require('MD5');
 
-var asset = function (filename) {
-  var content = fs.readFileSync(filename, 'utf-8');
-  var ext = path.extname(filename);
-  return md5(content) + ext;
-};
+function createAsset(assets) {
+  return function (filename) {
+    var content = fs.readFileSync(filename, 'utf-8');
+    var ext = path.extname(filename);
+    return assets + md5(content) + ext;
+  };
+}
 
 var middleware = function (param, next) {
-  param.context.asset = asset;
+  param.context.asset = createAsset(param.context.assets);
   next();
 };
 
