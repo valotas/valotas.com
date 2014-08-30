2531a4d4925a8fe31f9281a9ef321ce79dace094
Yoryos Valotasios <valotas@gmail.com>
2014-08-30 12:25:27 +0200

    Check that always the originalAssets has been provided

--- assemble/asset-middleware.spec.js
+++ assemble/asset-middleware.spec.js
@@ -7,19 +7,50 @@ var path = require('path');
 var fs = require('fs');
 var middleware = require('./asset-middleware');
 
+var commonContext = {
+  originalAssets: '/tmp',
+  assets: '/path/to/assets' 
+};
+
+var toThrowNamedMatcher = {
+  compare: function (actual, expected) {
+    try {
+      actual();
+      return {
+        pass: false,
+        message: 'Expected exception: "' + expected + '" but got nothing'
+      };
+    } catch (e) {
+      return {
+        pass: e.name === expected,
+        message: e.name + ' is not equal to ' + expected
+      };
+    }
+  }
+};
+
 function noop() {}
 
-function runMiddlewareAndGetAsset() {
+function runMiddlewareAndGetAsset(context) {
+  context = context || commonContext;
   var params = {
-    context: {
-      assets: '/path/to/assets'
-    }
+    context: commonContext
   };
   middleware(params, noop);
   return params.context.asset;
 }
 
 describe('asset-middleware', function () {
+  beforeEach(function () {
+    this.addMatchers({ toThrowNamed: toThrowNamedMatcher });
+  });
+  
+  it('should throw an exception if no originalAssets is present', function () {
+    expect(function () {
+      runMiddlewareAndGetAsset({}); 
+    }).toTrowNamed('IllegalContextException');
+  });
+  
   it('should add an asset() function to the given context', function () {
     var asset = runMiddlewareAndGetAsset();
     expect(asset).not.toBeUndefined();
