b2b724a304e9d3f46ef3d2a61b1deaa77bbb2d53
Georgios Valotasios <valotas@gmail.com>
2015-12-16 21:08:17 +0100

    Adapted the main.ts to create a dummy article

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -1,1 +1,20 @@
-console.log('This is main.ts');
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import {Article} from './content/Article';
+import {MdFile} from './content/MdFile';
+import {Layout} from './react/Layout';
+
+const mdfile = MdFile.create(`
+---
+title: Tomcat init.d script
+author: valotas
+date: 2011-05-14
+template: article.jade
+---
+
+The actual content
+
+`);
+const article = new Article(mdfile);
+
+ReactDom.render(React.createElement(Layout, {article: article}), document.body);
