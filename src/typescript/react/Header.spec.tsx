24d800f5cf2c9e98ac58e558ea06fc89ca4af576
Georgios Valotasios <valotas@gmail.com>
2015-12-25 13:13:44 +0100

    Fixed failing test

--- src/typescript/react/Header.spec.tsx
+++ src/typescript/react/Header.spec.tsx
@@ -6,6 +6,6 @@ describe('Header', () => {
 	it('should render html with the given title and subtitle',() => {
 		const html = RDS.renderToString(<Header title="The title" subtitle="The subtitle" />);
 		expect(html).toContain('The title</h1>');
-		expect(html).toContain('The subtitle</p>');
+		expect(html).toContain('The subtitle</span>');
 	});
 });
