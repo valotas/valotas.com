96811a84c50711327bcfbafa94c973c3b2f2281a
Georgios Valotasios <valotas@gmail.com>
2015-12-27 18:18:10 +0100

    Introduced utility function that creates html script tags

--- src/typescript/google.ts
+++ src/typescript/google.ts
@@ -1,13 +1,11 @@
+import {createScript} from './utils';
 
 export function loadWebfonts() {
 	window['WebFontConfig'] = {
 		google: { families: [ 'Gloria+Hallelujah::latin', 'Open+Sans::latin,greek' ] }
 	};
     
-	const wf = document.createElement('script');
-    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = true;
+	const wf = createScript('//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js');
     document.getElementsByTagName('body')[0]
 		.appendChild(wf);
 }
