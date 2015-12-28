02aa1024731c7c86a7b1b640e96db01b6d7dc169
Georgios Valotasios <valotas@gmail.com>
2015-12-28 22:09:17 +0100

    Introduced a custom marked renderer that can create react components

--- src/typescript/react/MarkedRenderer.spec.ts
+++ src/typescript/react/MarkedRenderer.spec.ts
@@ -1,21 +1,54 @@
 import * as React from 'react';
 import * as RDS from 'react-dom/server';
 import * as marked from 'marked';
-import {MarkedRenderer} from './MarkedRenderer';
+import {createComponentTree} from './MarkedRenderer';
 
-describe('MarkedRenderer', () => {
-	let renderer;
-	
-	beforeEach(() => {
-			renderer = new MarkedRenderer();
+fdescribe('MarkedRenderer', () => {
+	it('should render headers',() => {
+		const rendered = createComponentTree('# head');
+		const html = RDS.renderToStaticMarkup(rendered);
+		expect(html).toContain('<h1>head</h1>');
 	});
 	
-	it('should render html',() => {
-		const parsed = marked.lexer('# head\n\nparagraph');
-		const rendered = renderer.render(parsed);
-		const html = RDS.renderToString(rendered);
-		expect(html).toContain('head</h1>');
-		expect(html).toContain('paragraph</p>');
+	it('should render headers with emphasized text',() => {
+		const rendered = createComponentTree('# this _is_ important');
+		const html = RDS.renderToStaticMarkup(rendered);
+		expect(html).toContain('<em>is</em>');
 	});
 	
+	it('should render paragraphs with strong or emphasized text',() => {
+		const rendered = createComponentTree('this a **very strong** _paragraph_!');
+		const html = RDS.renderToStaticMarkup(rendered);
+		expect(html).toContain('<strong>very strong</strong>');
+		expect(html).toContain('<em>paragraph</em>');
+	});
+	
+	it('should render more than one block elements',() => {
+		const rendered = createComponentTree('## header \n\nthis a _paragraph_!');
+		const html = RDS.renderToStaticMarkup(rendered);
+		expect(html).toContain('<h2>header</h2>');
+		expect(html).toContain('<em>paragraph</em>');
+	});
+	
+	it('should render code spans',() => {
+		const rendered = createComponentTree('this a `a code`!');
+		const html = RDS.renderToStaticMarkup(rendered);
+		expect(html).toContain('<code>a code</code>');
+	});
+	
+	it('should render code blocks with a class derived from the language',() => {
+		const rendered = createComponentTree('this a\n\n```js\na code block\n```');
+		const html = RDS.renderToStaticMarkup(rendered);
+		expect(html).toContain('<pre><code class="lang-js">a code block</code></pre>');
+	});
+	
+	it('should quotes with paragraphs',() => {
+		const rendered = createComponentTree(`
+> this is a quote paragraph
+> this is another paragraph		
+		`);
+		const html = RDS.renderToStaticMarkup(rendered);
+		expect(html).toContain('<blockquote><p this is a quote paragraph</p>');
+		expect(html).toContain('<p> this is another paragraph</p></blockquote>');
+	});
 });
--- src/typescript/react/MarkedRenderer.tsx
+++ src/typescript/react/MarkedRenderer.tsx
@@ -1,17 +1,122 @@
+import * as marked from 'marked';
 import * as React from 'react';
+import * as ex from '../exceptions';
 
-export class MarkedRenderer {
-	heading(text: string, level?: number) {
-		return <h1>{text}</h1>;
-	}
-	paragraph(text: string) {
-		return <p>{text}</p>;
+//https://github.com/christianalfoni/markdown-to-react-components/blob/master/src/index.js
+
+const R = React.DOM;
+
+class MarkedReactRenderer {
+	private tree = [];
+	private index = 0;
+	private inlineTree = [];
+	
+	constructor () {
+		
 	}
 	
-	render(tokens: any[]): React.ReactElement<any> {
-		return <div>{tokens.map(this.renderToken.bind(this))}</div>;
+	code(code: string, language: string) {
+		this.codespan(code, language);
+		this.pushBlock(R.pre);
 	}
-	renderToken(token) {
-		return this[token.type](token.text);
-	} 
+	
+    blockquote(quote: string) {
+		console.log('blockquote', quote);
+	}
+	
+    html(html: string) {
+		
+	}
+	
+    heading(text: string, level: number) {
+		const el = 'h' + level;
+		this.pushBlock(R[el]);
+	}
+	pushBlock(factory, props = {key: '0'}) {
+		this.index = this.index + 1;
+		props.key = this.index + '';
+		const childs = firstChildOrFullArray(this.inlineTree);
+		const el = factory(props, childs);
+		this.inlineTree = [];
+		this.tree.push(el);
+	}
+    hr() {
+		this.pushBlock(R.hr);
+	}
+    list(body: string, ordered: boolean) {
+		
+	}
+    listitem(text: string) {
+		
+	}
+    paragraph(text: string) {
+		this.pushBlock(R.p);
+	}
+    table(header: string, body: string) {
+		//not implemented yet
+	}
+    tablerow(content: string) {
+		//not implemented yet
+	}
+    tablecell(content: string, flags: {
+        header: boolean,
+        align: string
+    }) {
+		//not implemented yet
+	}
+    strong(text: string) {
+		this.pushInline(R.strong, text);
+	}
+    em(text: string) {
+		this.pushInline(R.em, text);
+	}
+	pushInline(el, text, props = null) {
+		this.inlineTree.pop();
+		this.inlineTree.push(el(props, text));
+	}
+    codespan(code: string, lang?: string) {
+		const props = lang ? {
+			className: 'lang-' + lang
+		} : null;
+		this.pushInline(R.code, code, props);
+	}
+    br() {
+		
+	}
+    del(text: string) {
+		
+	}
+    link(href: string, title: string, text: string) {
+		
+	}
+    image(href: string, title: string, text: string) {
+		
+	}
+	text(text: string) {
+		this.inlineTree.push(text);
+		return text;
+	}
+	
+	createComponentTree(html: string) {
+		//const tokens = marked.lexer(html);
+		//const parser = marked.parser(tokens);
+		//console.log(parser);
+		marked(html, {
+			renderer: this,
+			smartypants: true
+		});
+		return <div>{firstChildOrFullArray(this.tree)}</div>;
+	}
+}
+
+function firstChildOrFullArray(input: any[]) {
+	if (input && input.length === 1) {
+		return input[0];
+	}
+	return input;
+}
+
+export function createComponentTree(html: string): React.ReactElement<any> {
+	const renderer = new MarkedReactRenderer();
+	return renderer.createComponentTree(html);
 }
