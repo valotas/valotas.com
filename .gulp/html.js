b7b86ff347cfb47ed286e584c0333c088da813f9
Georgios Valotasios <valotas@gmail.com>
2016-01-02 10:25:46 +0100

    Simplified the html creation

--- .gulp/html.js
+++ .gulp/html.js
@@ -1,9 +1,5 @@
 'use strict';
 
-var gulpif = require('gulp-if'), 
-  htmlreplace = require('gulp-html-replace'),
-  htmlmin = require('gulp-htmlmin');
-
 module.exports = function (gulp) {
   return function () {
     var plugin = require('../build/typescript/gulp');
@@ -16,11 +12,6 @@ module.exports = function (gulp) {
       .pipe(plugin.addMetafiles())
       .pipe(plugin.adaptPaths())
       .pipe(plugin.wrapHtml('src/templates/index.jade'))
-      .pipe(gulpif('*.html', htmlreplace({
-        'css': 'assets/main.css',
-        'js': 'typescript/main.js'
-      })))
-      .pipe(gulpif('*.html', htmlmin()))
       .pipe(gulp.dest('build'))
   };
 };
