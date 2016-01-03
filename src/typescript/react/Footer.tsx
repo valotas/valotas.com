04e45e033c5b1550a720bcf4fda5616d1bf1f781
Georgios Valotasios <valotas@gmail.com>
2016-01-03 17:44:51 +0100

    layout fix of the Footer

--- src/typescript/react/Footer.tsx
+++ src/typescript/react/Footer.tsx
@@ -7,15 +7,15 @@ export class Footer extends React.Component<any, {}> {
 			<div id="footer">
 				<div className="container text-center">
 					<div className="col-md-12">
-						&copy; Γιώργος Βαλοτάσιος - CSS by 
+						&copy; Γιώργος Βαλοτάσιος - CSS by &nbsp; 
 						<a href="https://twitter.com/MrPirrera" target="_blank">@MrPirrera</a>
 					</div>
 					<div className="col-sm-12 col-md-1">
-						<Icon name="fa-asterisk" size="2x"/>
+						<Icon name="fa-asterisk" />
 					</div>
 					<div className="col-sm-12 col-md-11 whitebg">
 						The greek name 
-						<strong><em>Γιώργος</em></strong>
+						"<strong><em>Γιώργος</em></strong>"
 						is also know as Yoryos, Georgios or just George which seems to be easier to most english speaking people.
 						If you are trying to find out what Βαλοτασιος means, just think of it as Valotasios and you should be fine.
 					</div>
