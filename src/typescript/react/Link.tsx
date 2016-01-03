9e0aa22ed527f77c959c0caa0a13bbdd067a711e
Georgios Valotasios <valotas@gmail.com>
2016-01-03 19:05:39 +0100

    Made the Link a stateless functional component

--- src/typescript/react/Link.tsx
+++ src/typescript/react/Link.tsx
@@ -11,45 +11,41 @@ interface LinkProps extends React.Props<any> {
 	target?: string;
 }
 
-export class Link extends React.Component<LinkProps, any> {
-	context: {
-		metafileStore: MetaFileStore
-	};
+export function Link (props: LinkProps, context: {metafileStore?: MetaFileStore}) {
+	const href = props.href || createHref(props.article);
+	const onclick = handleClick.bind(null, props, context);
+	return <a href={href} className={props.className} onClick={onclick} target={props.target}>{props.children}</a>;
+}
 
-	static contextTypes: React.ValidationMap<any> = {
-		metafileStore: React.PropTypes.object
-	};
-	
-	handleClick (e) {
-		const {href, target} = this.props;
-		if (href === '#') {
-			e.preventDefault();
-			window.scrollTo(0, 0);
-			return;
-		}
-		
-		if (target === '_blank' || (href && href.indexOf('http') === 0)) {
-			return;
-		}
+Link.prototype.contextTypes = {
+	metafileStore: React.PropTypes.object
+};
 
-		const store = this.props.metafileStore || this.context.metafileStore;
-		if (!store) {
-			return;
-		}
-		
+function handleClick (props: LinkProps, context,  e) {
+	const {href, target} = props;
+	if (href === '#') {
 		e.preventDefault();
-		store.load(this.props.article || this.props.href || '/');
+		window.scrollTo(0, 0);
+		return;
 	}
-	render() {
-		const href = this.props.href || this.createHref();
-		return <a href={href} className={this.props.className} onClick={this.handleClick.bind(this)} target={this.props.target}>{this.props.children}</a>;
+	
+	if (target === '_blank' || (href && href.indexOf('http') === 0)) {
+		return;
 	}
-	createHref() {
-		let href = '/';
-		const article = this.props.article;
-		if (article) {
-			href += article.key;
-		}
-		return href;
+
+	const store = props.metafileStore || context.metafileStore;
+	if (!store) {
+		return;
 	}
+	
+	e.preventDefault();
+	store.load(props.article || href || '/');
+}
+
+function createHref(article?: ArticleDescription) {
+	let href = '/';
+	if (article) {
+		href += article.key;
+	}
+	return href;
 }
