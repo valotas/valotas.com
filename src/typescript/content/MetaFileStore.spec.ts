bb1438928601a1b35afb7b96a98cb496be025516
Georgios Valotasios <valotas@gmail.com>
2016-05-14 11:06:03 +0200

    tslint warnings fixes for MetaFileStore

--- src/typescript/content/MetaFileStore.spec.ts
+++ src/typescript/content/MetaFileStore.spec.ts
@@ -4,27 +4,27 @@ import {MetaFile} from './MetaFile';
 describe('MetaFileStore', () => {
     let fetcher;
     let store: MetaFileStore;
-    
+
     beforeEach(() => {
         fetcher = {fetch: function () {
-            
+
         }};
         store = new MetaFileStore(fetcher);
     });
-    
+
     it('should have a way to add new listeners', () => {
         store.onChange(function() {});
     });
-    
+
     it('should have provide a way to load new metafiles', () => {
         const meta = new MetaFile();
         spyOn(store, '_loadMetaFile').and.returnValue(Promise.resolve(meta));
 
         store.load('123');
-        
+
         expect(store._loadMetaFile).toHaveBeenCalledWith('/123/meta.json');
     });
-    
+
     it('should notify the listeners of newly loaded metafiles', (done) => {
         const meta = new MetaFile();
         const listener = jasmine.createSpy('listener');
@@ -33,11 +33,11 @@ describe('MetaFileStore', () => {
         store.onChange(listener);
         store.load('123')
             .then(() => {
-                expect(listener).toHaveBeenCalledWith(meta); 
+                expect(listener).toHaveBeenCalledWith(meta);
             })
             .then(done);
     });
-    
+
     it('should return a function to deregister listener', (done) => {
         const meta = new MetaFile();
         const listener = jasmine.createSpy('listener');
@@ -46,54 +46,54 @@ describe('MetaFileStore', () => {
         const deregister = store.onChange(listener);
         expect(deregister).toBeTruthy();
         deregister();
-        
+
         store.load('123')
             .then(() => {
-                expect(listener).not.toHaveBeenCalledWith(meta); 
+                expect(listener).not.toHaveBeenCalledWith(meta);
             })
             .then(done);
     });
-    
+
     describe('_createUrl', () => {
        it('should add the meta.json at the end if the path is the root: /', () => {
            const actual = store._createUrl('/');
            expect(actual).toEqual('/meta.json');
        });
-       
+
        it('should create urls starting with /', () => {
            const actual = store._createUrl('key');
            expect(actual).toEqual('/key/meta.json');
        });
-       
+
        it('should not duplicate trailing /', () => {
            const actual = store._createUrl('key/');
            expect(actual).toEqual('/key/meta.json');
        });
     });
-    
+
     describe('_loadMetaFile', () => {
-        const meta1:MetaFileData = {
+        const meta1: MetaFileData = {
             title: 'title1',
             path: 'path1',
             date: null
         };
 
-        const meta2:MetaFileData = {
+        const meta2: MetaFileData = {
             title: 'title2',
             path: 'path2',
             date: null
-        }
-        
+        };
+
         let body;
-       
+
         it('should fetch the meta.json', (done) => {
             body = {
                 json: function () {
-                    return Promise.resolve(meta1)
+                    return Promise.resolve(meta1);
                 }
-            }
+            };
             spyOn(fetcher, 'fetch').and.returnValue(Promise.resolve(body));
-           
+
             store
                 .load('any')
                 .then((actual) => {
@@ -102,16 +102,15 @@ describe('MetaFileStore', () => {
                 })
                 .then(done);
        });
-       
-       
+
        it('should also allow arrays of metafiles', (done) => {
            body = {
                json: function () {
-                   return Promise.resolve([meta1, meta2])
+                   return Promise.resolve([meta1, meta2]);
                }
-           }
+           };
            spyOn(fetcher, 'fetch').and.returnValue(Promise.resolve(body));
-           
+
            store
                 .load('any')
                 .then((actual) => {
--- src/typescript/content/MetaFileStore.ts
+++ src/typescript/content/MetaFileStore.ts
@@ -3,17 +3,17 @@ import {Article} from './Article';
 import {isString} from '../utils';
 import * as ex from '../exceptions';
 
-function isArticle(input:any): input is Article {
+function isArticle(input: any): input is Article {
 	return input.key;
-} 
+}
 
 export class MetaFileStore {
 	private listeners: Function[] = [];
-	
+
 	constructor(private fetcher: Fetcher) {
-		
+
 	}
-	
+
 	load(input: string|Article) {
 		const url = this._createUrl(input);
 		return this._loadMetaFile(url)
@@ -21,8 +21,8 @@ export class MetaFileStore {
 				return this.setMetaFile(meta);
 			});
 	}
-	
-	_createUrl(input:string|Article) {
+
+	_createUrl(input: string|Article) {
 		if (isString(input)) {
 			let url = input as string;
 			if (url.indexOf('/') !== 0) {
@@ -34,23 +34,23 @@ export class MetaFileStore {
 			return url + 'meta.json';
 		}
 		if (isArticle(input)) {
-			return '/' + input.key + '/meta.json';	
+			return '/' + input.key + '/meta.json';
 		}
 		throw ex.illegalFromatException('Can not create a url from given input: ' + input);
 	}
-	
+
 	_loadMetaFile(url: string) {
 		return this.fetcher
 			.fetch(url)
 			.then((body) => body.json())
 			.then((json) => MetaFile.fromData(json as MetaFileData|MetaFileData[]));
 	}
-	
+
 	setMetaFile(meta: MetaFile|MetaFile[]) {
 		this.listeners.forEach((listener) => listener(meta));
 		return meta;
 	}
-	
+
 	onChange(listener: (meta: MetaFile|MetaFile[]) => void) {
 		this.listeners.push(listener);
 		return () => {
