d044cc768002476b43d321db482f6da9f610a7a4
Georgios Valotasios <valotas@gmail.com>
2016-05-14 21:40:00 +0200

    Switched to react functional component and fixed tslint warnings for ParagraphWithFirstLetterSpan

--- src/typescript/react/ParagraphWithFirstLetterSpan.tsx
+++ src/typescript/react/ParagraphWithFirstLetterSpan.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 
-export class ParagraphWithFirstLetterSpan extends React.Component<any, any> {	
-	render() {
-        const childs = React.Children.toArray(this.props.children);
-        const child = childs[0] as string;
-        const first = child[0];
-        childs[0] = child.substr(1);
-		return <p><span className="first-letter">{first}</span>{childs}</p>;
-	}
+export function ParagraphWithFirstLetterSpan (props) {
+	const childs = React.Children.toArray(this.props.children);
+    const child = childs[0] as string;
+    const first = child[0];
+    childs[0] = child.substr(1);
+    return <p><span className='first-letter'>{first}</span>{childs}</p>;
 }
