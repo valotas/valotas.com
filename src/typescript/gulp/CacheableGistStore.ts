15d84ff34a13537b96d105ceda53fcd3c2edeeae
Georgios Valotasios <valotas@gmail.com>
2016-05-14 11:00:48 +0200

    tslint wanting fixes

--- src/typescript/gulp/CacheableGistStore.ts
+++ src/typescript/gulp/CacheableGistStore.ts
@@ -3,17 +3,17 @@ import {GistStore} from '../content/GistStore';
 
 export class CacheableGistStore extends GistStore {
 	private promises: {[k: string]: Promise<string>} = {};
-	
+
 	constructor(public fetcher: Fetcher, meta: MetaFileData) {
 		super(fetcher, null, meta);
 	}
-	
+
 	_load(url: string) {
 		const promise = this.promises[url] || super._load(url);
 		this.promises[url] = promise;
 		return promise;
 	}
-	
+
 	all () {
 		return Promise.all(Object.keys(this.promises).map((k) => this.promises[k]));
 	}
--- src/typescript/gulp/NodeFetcher.ts
+++ src/typescript/gulp/NodeFetcher.ts
@@ -3,19 +3,19 @@ import nfetch = require('node-fetch');
 export class NodeFetcher implements Fetcher {
 	private promises: {[k: string]: Promise<Response>} = {};
 	_fetch;
-	
+
 	constructor(delegate: Fetcher) {
 		this._fetch = delegate ? delegate.fetch : nfetch;
 	}
-	
+
 	fetch (url: string|Request, init?: RequestInit) {
 		const key = url as string;
 		console.log(key, this.promises[key] ? true : false);
 		const promise = this.promises[key] || this._fetch(url, init);
-		this.promises[key] = promise; 
+		this.promises[key] = promise;
 		return promise;
 	}
-	
+
 	all () {
 		return Promise.all(Object.keys(this.promises).map((k) => this.promises[k]));
 	}
