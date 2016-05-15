ab66a9f58c71188a73357ed81943083ecd622054
Georgios Valotasios <valotas@gmail.com>
2016-05-15 19:26:21 +0200

    Exported the listener/subscribe/unsubscribe to the Bus utility classs

---
+++ src/typescript/Bus.ts
@@ -0,0 +1,20 @@
+
+export class Bus<T> {
+    private listeners: ((T) => void)[] = [];
+    
+    notify (val: T) {
+        this.listeners.forEach((listener) => {
+            listener(val);
+        });
+    }
+    
+    register(listener: (T) => void) {
+        this.listeners.push(listener);
+        
+        // return an unregister function
+        return () => {
+            const index = this.listeners.indexOf(listener);
+			this.listeners.splice(index, 1);
+        };
+    }
+}
--- src/typescript/FetchStreamer.ts
+++ src/typescript/FetchStreamer.ts
@@ -1,28 +1,23 @@
 import {BROWSER} from './browser/Browser';
+import {Bus} from './Bus';
 
 interface ResponseListener {
     (promise: Promise<Response>): void;
 }
 
 export class FetchStreamer implements Fetcher {
-    private listeners: ResponseListener[] = [];
+    private bus: Bus<Promise<Response>> = new Bus();
 
     constructor (private delegate: Fetcher = BROWSER) {}
 
     fetch (url: string|Request, init?: RequestInit) {
         const promise = this.delegate.fetch(url, init);
-        this.listeners.forEach((l) => {
-            l(promise);
-        });
+        this.bus.notify(promise);
         return promise;
     }
 
     onFetch (listener: ResponseListener) {
-        this.listeners.push(listener);
-        return () => {
-            const index = this.listeners.indexOf(listener);
-			this.listeners.splice(index, 1);
-        };
+        return this.bus.register(listener);
     }
 
     static wrap (fetcher: Fetcher|FetchStreamer) {
