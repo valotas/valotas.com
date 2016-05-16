a3926930094fd659b6ea621ba62cac64915ff16d
Georgios Valotasios <valotas@gmail.com>
2016-05-16 11:00:02 +0200

    Fixed tslint errors

--- src/typescript/Bus.spec.ts
+++ src/typescript/Bus.spec.ts
@@ -11,28 +11,30 @@ describe('Bus', () => {
         const value = 'the value';
         const listener1 = jasmine.createSpy('listener1');
         const listener2 = jasmine.createSpy('listener2');
-        
+
         bus.register(listener1);
         bus.register(listener2);
 
-        //when
+        // when
         bus.notify(value);
-        
+
+        // then
         expect(listener1).toHaveBeenCalledWith(value);
         expect(listener2).toHaveBeenCalledWith(value);
     });
-    
+
     describe('register', () => {
        it('should return an unregister function', () => {
             const value = 'the value';
             const listener = jasmine.createSpy('listener1');
-            
+
             const registration = bus.register(listener);
 
-            //when
+            // when
             registration();
             bus.notify(value);
-            
+
+            // then
             expect(listener).not.toHaveBeenCalled();
        });
     });
--- src/typescript/Bus.ts
+++ src/typescript/Bus.ts
@@ -1,16 +1,16 @@
 
 export class Bus<T> {
     private listeners: ((T) => void)[] = [];
-    
+
     notify (val: T) {
         this.listeners.forEach((listener) => {
             listener(val);
         });
     }
-    
+
     register(listener: (T) => void) {
         this.listeners.push(listener);
-        
+
         // return an unregister function
         return () => {
             const index = this.listeners.indexOf(listener);
