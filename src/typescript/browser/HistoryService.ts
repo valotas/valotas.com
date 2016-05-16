423376ee56e64cc91f43d3a627be9fa4a4700283
Georgios Valotasios <valotas@gmail.com>
2016-05-16 10:53:30 +0200

    Introduced a historyService and it's tests

---
+++ src/typescript/browser/HistoryService.ts
@@ -0,0 +1,27 @@
+import {BROWSER} from './Browser';
+import {Bus} from '../Bus';
+
+export class HistoryService {
+	private popStateBus: Bus<PageState> = new Bus();
+	private pushStateBus: Bus<PageState> = new Bus();
+
+	constructor(private win: Window) {
+        win.addEventListener('onpopstate', (ev: PopStateEvent) => {
+			const page = ev.state as PageState;
+			this.popStateBus.notify(page);
+		});
+	}
+
+	pushState(page: PageState) {
+		this.win.history.pushState(page, page.title, page.path);
+		this.pushStateBus.notify(page);
+	}
+
+	onPushState(listener: (PageState) => void) {
+		return this.pushStateBus.register(listener);
+	}
+
+	onPopState(listener: (PageState) => void) {
+		return this.popStateBus.register(listener);
+	}
+}
