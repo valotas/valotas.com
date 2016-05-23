2154bb0c8fad54d6aa8fadc2e5a9f2b9b03a7515
Georgios Valotasios <valotas@gmail.com>
2016-05-23 22:59:14 +0200

    Include fontawsome to the sass paths

--- .gulp/css.js
+++ .gulp/css.js
@@ -13,7 +13,10 @@ module.exports = function (gulp, onChange) {
   return function () {
     return gulp.src('./src/sass/**/*.scss')
       .pipe(sass({
-        includePaths: './node_modules/foundation-sites/scss'
+        includePaths: [
+          './node_modules/foundation-sites/scss',
+          './node_modules/font-awesome/scss'
+        ]
       }).on('error', sass.logError))
       .pipe(cssmin())
       .pipe(gulp.dest('./build/assets'))
