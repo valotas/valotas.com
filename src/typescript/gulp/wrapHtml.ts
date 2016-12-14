eab2cbad303f2a57b63482f219b274bd4fc700c0
Georgios Valotasios <valotas@gmail.com>
2016-12-14 14:38:16 +0100

    Replaced jade with pug

--- src/typescript/gulp/wrapHtml.ts
+++ src/typescript/gulp/wrapHtml.ts
@@ -1,24 +1,24 @@
-import * as jade from 'jade';
+import { compileFile } from 'pug';
 import * as through from 'through2';
 import * as gutil from 'gulp-util';
-import {deflate} from '../utils';
-import {createTitle} from '../titleFactory';
-import {createPackageJson} from '../PackageJson.factory';
+import { deflate } from '../utils';
+import { createTitle } from '../titleFactory';
+import { createPackageJson } from '../PackageJson.factory';
 
 export function wrapHtml(templateFile, pkg, logger: Logger = gutil) {
-	const packageJson = createPackageJson(pkg);
-	const template = jade.compileFile(templateFile);
-	return through.obj(function (file: GulpFile, enc, callback) {
-		if (file.html) {
-			const html = template({
-				title: createTitle(file.meta || null),
-				content: file.html,
-				meta: deflate(file.meta),
-				pkg: `window.pkg=${JSON.stringify(packageJson)}`
-			});
-			file.contents = new Buffer(html, enc);
-            logger.log('Created', file.path);
-		}
-		callback(null, file);
-	});
+  const packageJson = createPackageJson(pkg);
+  const template = compileFile(templateFile);
+  return through.obj(function (file: GulpFile, enc, callback) {
+    if (file.html) {
+      const html = template({
+        title: createTitle(file.meta || null),
+        content: file.html,
+        meta: deflate(file.meta),
+        pkg: `window.pkg=${JSON.stringify(packageJson)}`
+      });
+      file.contents = new Buffer(html, enc);
+      logger.log('Created', file.path);
+    }
+    callback(null, file);
+  });
 }
