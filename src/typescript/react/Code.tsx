b41c5baaac33a5a46662beb6e1eb1200222c8475
Georgios Valotasios <valotas@gmail.com>
2016-12-18 11:47:10 +0100

    Reformatted Code and make use of spread properties

--- src/typescript/react/Code.tsx
+++ src/typescript/react/Code.tsx
@@ -1,41 +1,41 @@
 import * as React from 'react';
-import {GistStore} from '../content/GistStore';
-import {isPromise} from '../utils';
-import {Link} from './Link';
-import {Icon} from './Icon';
+import { GistStore } from '../content/GistStore';
+import { isPromise } from '../utils';
+import { Link } from './Link';
+import { Icon } from './Icon';
 
-export function Code (props) {
-	const {children} = props;
-	const title = Array.isArray(children) ? children[0] : null;
-	const code = Array.isArray(children) ? children[1] : children;
-	return (
-		<div className='codeblock'>
-			{createTitleBlock(title)}
-			{createPreBlock(props, code)}
-		</div>
-	);
+export function Code(props) {
+  const {children} = props;
+  const title = Array.isArray(children) ? children[0] : null;
+  const code = Array.isArray(children) ? children[1] : children;
+  return (
+    <div className='codeblock'>
+      {createTitleBlock(title)}
+      {createPreBlock(props, code)}
+    </div>
+  );
 }
 
 function createTitleBlock(title) {
-	if (!title) {
-		return null;
-	}
-	return (<div className='title'>{title}</div>);
+  if (!title) {
+    return null;
+  }
+  return (<div className='title'>{title}</div>);
 }
 
 function createPreBlock(props, code) {
-	const dataProps = {};
-	Object.keys(props).forEach((key) => {
-		if (key.indexOf('data-') === 0) {
-			dataProps[key] = props[key];
-		}
-	});
-	return React.DOM.pre(dataProps, createCodeBlock(code));
+  const dataProps = {};
+  Object.keys(props).forEach((key) => {
+    if (key.indexOf('data-') === 0) {
+      dataProps[key] = props[key];
+    }
+  });
+  return <pre {...dataProps}>{createCodeBlock(code)}</pre>;
 }
 
 function createCodeBlock(code) {
-	if (code.type === 'code') {
-		return code;
-	}
-	return (<code>{code}</code>);
+  if (code.type === 'code') {
+    return code;
+  }
+  return (<code>{code}</code>);
 }
