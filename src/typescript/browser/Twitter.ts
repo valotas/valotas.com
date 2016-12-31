b455b807ce8984e3147ba06bf40feaa94da80502
Georgios Valotasios <valotas@gmail.com>
2016-12-31 15:49:41 +0100

    Reformat

--- src/typescript/browser/Twitter.ts
+++ src/typescript/browser/Twitter.ts
@@ -1,49 +1,49 @@
-import {BROWSER} from './Browser';
+import { BROWSER } from './Browser';
 
 const TWITTER_SCRIPT_ID = 'twitter-wjs';
 const TWTTR = 'twttr';
 
 interface TwttrWidgets {
-	load();
+  load();
 }
 
 interface Twttr {
-	widgets: TwttrWidgets;
+  widgets: TwttrWidgets;
 }
 
 class TwitterThenable {
-    then(f: (t: Twttr) => void) {
-        const twttr = window[TWTTR];
-        if (twttr.widget) {
-            f(twttr as Twttr);
-        } else {
-            twttr.ready(() => {
-               f(window[TWTTR] as Twttr);
-            });
-        }
+  then(f: (t: Twttr) => void) {
+    const twttr = window[TWTTR];
+    if (twttr.widget) {
+      f(twttr as Twttr);
+    } else {
+      twttr.ready(() => {
+        f(window[TWTTR] as Twttr);
+      });
     }
+  }
 }
 
 export function loadTwitter(browser = BROWSER) {
-    const {window} = browser;
+  const {window} = browser;
 
-	const loaded = window[TWTTR];
-	if (!loaded) {
-        window[TWTTR] = createTwttr(window);
+  const loaded = window[TWTTR];
+  if (!loaded) {
+    window[TWTTR] = createTwttr(window);
 
-		// load the widgets.js
-		browser.addScript('//platform.twitter.com/widgets.js', {
-			id: TWITTER_SCRIPT_ID,
-			protocol: 'https'
-		});
-	}
-	return new TwitterThenable();
+    // load the widgets.js
+    browser.addScript('//platform.twitter.com/widgets.js', {
+      id: TWITTER_SCRIPT_ID,
+      protocol: 'https'
+    });
+  }
+  return new TwitterThenable();
 }
 
 function createTwttr(window) {
-    const t = window[TWTTR] || { _e: [] };
-    t.ready = function (f) {
-        t._e.push(f);
-    };
-    return t;
+  const t = window[TWTTR] || { _e: [] };
+  t.ready = function (f) {
+    t._e.push(f);
+  };
+  return t;
 }
