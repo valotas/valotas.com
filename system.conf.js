29d46cc5af70352b038f2745309bc793b5973006
Georgios Valotasios <valotas@gmail.com>
2017-03-20 21:30:44 +0100

    Replaced react with preact mappings

--- system.conf.js
+++ system.conf.js
@@ -4,25 +4,24 @@
 'use strict';
 
 (function (g) {
-  
+
   function createConfig(prefix) {
     var pre = prefix || '/';
     return {
       defaultJSExtensions: true,
       map: {
-        'react': pre + 'node_modules/react/dist/react.min.js',
-        'react-dom': pre + 'node_modules/react-dom/dist/react-dom.min.js',
+        'preact': pre + 'node_modules/preact/dist/preact.dev.js',
         'moment': pre + 'node_modules/moment/min/moment.min.js',
         'marked': pre + 'node_modules/marked/marked.min.js',
         'base64-js': pre + 'node_modules/base64-js/lib/b64.js',
-        'tslib': pre + 'node_modules/tslib/tslib.js' 
-      }  
+        'tslib': pre + 'node_modules/tslib/tslib.js'
+      }
     };
   }
-  
+
   if (g.System) {
     g.System.config(createConfig());
   } else  {
-    g.createSystemConfig = createConfig; 
+    g.createSystemConfig = createConfig;
   }
 })(typeof window !== 'undefined' ? window : module.exports);
