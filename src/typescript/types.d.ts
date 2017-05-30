8f412169eed36a068c9d900a157490dd10cb536f
Georgios Valotasios <valotas@gmail.com>
2017-05-30 20:07:28 +0200

    Fix linting problems

--- src/typescript/types.d.ts
+++ src/typescript/types.d.ts
@@ -1,4 +1,4 @@
-declare module "base64-js" {
+declare module 'base64-js' {
   export function byteLength(encoded: string): number;
   // export function toByteArray(encoded: string): Uint8Array;
   export function fromByteArray(bytes: Uint8Array | number[]): string;
