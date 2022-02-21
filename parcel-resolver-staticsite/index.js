a9ef083977ad99e17a84b8a9ef8152b2ed42b113
Georgios Valotasios <valotas@gmail.com>
2022-02-21 14:01:03 +0100

    feat: add a resolver for the index file

---
+++ parcel-resolver-staticsite/index.js
@@ -0,0 +1,44 @@
+/* eslint-env node */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { Resolver } = require("@parcel/plugin");
+const path = require("path");
+
+// function createPathRange(start, end) {
+//   const paths = [];
+
+//   let current = start;
+//   while (current !== end) {
+//     current = path.join(current, "..");
+//     paths.push(current);
+//   }
+
+//   return paths;
+// }
+
+exports.default = new Resolver({
+  async resolve({ logger, options, specifier, dependency }) {
+    if (!dependency.resolveFrom.toLowerCase().endsWith(".md")) {
+      return null;
+    }
+
+    const file = options.inputFS.findAncestorFile(
+      [specifier],
+      dependency.resolveFrom,
+      options.projectRoot
+    );
+
+    if (file) {
+      const filePath = await options.inputFS.realpath(file);
+      logger.log({ message: `found2: ${filePath}` });
+      return {
+        filePath,
+        sideEffects: true,
+        // invalidateOnFileCreate: [],
+        // invalidateOnFileChange: [],
+      };
+    }
+
+    return null;
+  },
+});
