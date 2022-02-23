eb3ce985de63d449f1314e7bacbbaf3e842b8086
Georgios Valotasios <valotas@gmail.com>
2022-02-23 16:41:09 +0100

    Remove unused stuff

--- web/src/typescript/titleFactory.ts
+++
@@ -1,9 +0,0 @@
-import { MetaFileData } from "./types";
-import { VALOTAS, isArray } from "./utils";
-
-export function createTitle(meta?: MetaFileData | MetaFileData[]) {
-  if (!isArray(meta)) {
-    return `${meta.title} - ${VALOTAS}`;
-  }
-  return `${VALOTAS} - Things to remember | Programming stuff :)`;
-}
