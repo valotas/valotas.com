f874e67bb57bcca96d0884fdccd257105fa3e37b
Georgios Valotasios <valotas@gmail.com>
2022-02-24 18:35:42 +0100

    feat: responsive footer

--- frontend/src/Footer.tsx
+++ frontend/src/Footer.tsx
@@ -11,28 +11,24 @@ export interface FooterProps {
 export function Footer({ pkgVersion }: FooterProps) {
   return (
     <div
-      className={tw`mt-12 text-gray-500 flex justify-between items-center`}
+      className={tw`flex flex-col mt-12 text-gray-500 `}
       data-testid="footer"
     >
-      <div
-        className={tw`pr-4 md:pr-6 lg:pr-12 flex justify-between items-center`}
-      >
-        <div className={tw`w-10 md:w-12 lg:w-20`}>
-          <Icon name="asterisk" className={tw`fill-gray-500  m-auto`} />
-        </div>
+      <div className={tw`md:pl-16 md:ml-2 lg:pl-32 lg:text-center`}>
+        valotas.com <strong>v{pkgVersion || ""}</strong> &copy; Γιώργος
+        Βαλοτάσιος - CSS inspired by{` `}
+        <Link href="https://adamwathan.me/" target="_blank">
+          Adam Wathan's
+        </Link>
+        {` `}
+        blog
       </div>
-
-      <div>
-        <div>
-          valotas.com <strong>v{pkgVersion || ""}</strong> &copy; Γιώργος
-          Βαλοτάσιος - CSS inspired by{` `}
-          <Link href="https://adamwathan.me/" target="_blank">
-            Adam Wathan's
-          </Link>
-          {` `}
-          blog
+      <div className={tw`flex flex-col md:flex-row mt-2`}>
+        <div className={tw`pr-4 md:pr-6 lg:pr-12 flex items-center`}>
+          <div className={tw`w-10 m-auto md:w-12 lg:w-20`}>
+            <Icon name="asterisk" className={tw`fill-gray-500`} />
+          </div>
         </div>
-
         <div>
           The greek name "
           <strong>
