5c5a89a496eb81af144888907f86daf64b295417
Georgios Valotasios <valotas@gmail.com>
2024-06-26 21:53:40 +0200

    fix: remove `ts-jest`

--- packages/parcel-transformer-staticsite/jest.config.mjs
+++ packages/parcel-transformer-staticsite/jest.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('jest').Config} */
 const config = {
   modulePathIgnorePatterns: ["dist"],
-  preset: "ts-jest",
   testEnvironment: "jsdom",
-  globals: {
-    "ts-jest": {
-      useESM: true,
-    },
-  },
   transform: {
     "^.+\\.(t|j)sx?$": ["@swc/jest"],
   },
